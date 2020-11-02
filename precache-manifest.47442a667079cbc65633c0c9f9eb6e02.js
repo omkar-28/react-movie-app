d9409df740e3a4358eb01a92b79d11b5fbe4d05b
omkar <omikhatavkar2@gmail.com>
2020-11-02 15:39:15 +0530

    Updates

---
+++ precache-manifest.47442a667079cbc65633c0c9f9eb6e02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e8c7f3ea283567e05a2a2b490c6dfbe",
+    "url": "/react-movieApp/index.html"
+  },
+  {
+    "revision": "56bbce6da2a08523d787",
+    "url": "/react-movieApp/static/css/main.2856f0f6.chunk.css"
+  },
+  {
+    "revision": "298d02e01383ec1537b4",
+    "url": "/react-movieApp/static/js/2.66c41520.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movieApp/static/js/2.66c41520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56bbce6da2a08523d787",
+    "url": "/react-movieApp/static/js/main.5cdce88b.chunk.js"
+  },
+  {
+    "revision": "74ddee5f5e08fbfc9bf8",
+    "url": "/react-movieApp/static/js/runtime-main.27284bdb.js"
+  }
+]);
